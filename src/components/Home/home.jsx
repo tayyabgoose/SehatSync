15f497f9ba98fe1703098f7955012f113fe8f54b
Muhammad Tayyab <40181908+tayyabgoose@users.noreply.github.com>
2024-05-10 23:38:14 +0500

    install: shadcn

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,22 +1,41 @@
 import "./home.css";
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const home = () => {
   return (
-    <div className="box">
-      <div className="hero-header-section">
-        <div className="overlap-group">
-          <div className="text-wrapper">
-            <img
-              className="text-wrapper"
-              src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            ></img>
+    <>
+      <div className="box">
+        <div className="hero-header-section">
+          <div className="overlap-group">
+            <div className="text-wrapper">
+              <img
+                className="text-wrapper"
+                src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              ></img>
+            </div>
           </div>
+          <div className="title">We Help You Keep in Sync with Your Sehat!</div>
+          <p className="lead">Never forget your medicines again.</p>
+          <div>
+            <Button>Let's Get Started</Button>
+          </div>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
-        <div className="title">We Help You Keep in Sync with Your Sehat!</div>
-        <p className="lead">Never forget your medicines again.</p>
-        <button className="cta">Let's Get Started</button>
       </div>
-    </div>
+    </>
   );
 };
 
